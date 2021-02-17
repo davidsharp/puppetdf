449b3bbe487945aff97e6644e3f02e5001a87174
davidsharp <hello@davidsharp.codes>
2021-02-17 12:59:33 +0000

    Split PDF request function out, add secret Chrome 84 flag

---
+++ pdf-request.js
@@ -0,0 +1,44 @@
+const pupLatest = require('puppeteer');
+const pup84 = require('puppeteer-chrome-84');
+
+function pdfRequest(request, response) {
+  try {
+    // hidden Chrome 84 (w/ export tagged pdf flag)
+    const pup = request.query.eightyfour ? pup84 : pupLatest
+    if (!request.query.html)
+      (async () => {
+        const browser = await pup.launch({
+          args: ['--no-sandbox', '--export-tagged-pdf']
+        });
+        const url = request.query.page ? decodeURIComponent(request.query.page) : null;
+        const urlWithHttps = /(^https?:\/\/)/g.test(url) ? url : 'https://' + url
+        const page = await browser.newPage();
+        try {
+          await page.goto(url ? urlWithHttps : 'https://google.com');
+        } catch (e) { response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ ' + e) }
+        const options = { format: request.query.size || 'A4', printBackground: typeof request.query.bg != undefined ? request.query.bg == 'true' : true }
+        const pdf = await page.pdf(options);
+        await browser.close();
+        response.type('application/pdf');
+        response.send(pdf);
+      })();
+    else
+      (async () => {
+        const browser = await pup.launch({
+          args: ['--no-sandbox', '--export-tagged-pdf']
+        });
+        const html = decodeURIComponent(request.query.html);
+        const page = await browser.newPage();
+        try {
+          await page.setContent(html);
+        } catch (e) { response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ ' + e) }
+        const options = { format: request.query.size || 'A4', printBackground: typeof request.query.bg != undefined ? request.query.bg == 'true' : true }
+        const pdf = await page.pdf(options);
+        await browser.close();
+        response.type('application/pdf');
+        response.send(pdf);
+      })();
+  } catch (e) { response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ ' + e) }
+}
+
+module.exports = pdfRequest
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const app = express();
 const pup = require('puppeteer');
 
+const pdfRequest = require('./pdf-request');
+
 let VERSION=null
 
 pup.launch({
@@ -27,78 +29,9 @@ app.get('/', function(request, response) {
   response.render('index',{chromium:VERSION})
 });
 
-app.get('/pdf', function(request, response) {try{
-  if(!request.query.html)
-(async () => {
-  const browser = await pup.launch({
-      args: ['--no-sandbox']
-    });
-  const url = request.query.page?decodeURIComponent(request.query.page):null;
-  const urlWithHttps = /(^https?:\/\/)/g.test(url)?url:'https://'+url
-  const page = await browser.newPage();
-  try{
-   await page.goto(url?urlWithHttps:'https://google.com');
-  }catch(e){response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ '+e)}
-  const options = {format:request.query.size||'A4', printBackground:typeof request.query.bg!=undefined?request.query.bg=='true':true}
-  const pdf = await page.pdf(options);
-  await browser.close();
-  response.type('application/pdf');
-  response.send(pdf);
-})();
-  else
-(async () => {
-  const browser = await pup.launch({
-      args: ['--no-sandbox']
-    });
-  const html = decodeURIComponent(request.query.html);
-  const page = await browser.newPage();
-  try{
-   await page.setContent(html);
-  }catch(e){response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ '+e)}
-  const options = {format:request.query.size||'A4', printBackground:typeof request.query.bg!=undefined?request.query.bg=='true':true}
-  const pdf = await page.pdf(options);
-  await browser.close();
-  response.type('application/pdf');
-  response.send(pdf);
-})();
-}catch(e){response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ '+e)}});
-
+app.get('/pdf', pdfRequest);
 //secret bonus feature, use any string to set a default PDF name
-app.get(/.*/, function(request, response) {try{
-  if(!request.query.html)
-(async () => {
-  const browser = await pup.launch({
-      args: ['--no-sandbox']
-    });
-  const url = request.query.page?decodeURIComponent(request.query.page):null;
-  const urlWithHttps = /(^https?:\/\/)/g.test(url)?url:'https://'+url
-  const page = await browser.newPage();
-  try{
-   await page.goto(url?urlWithHttps:'https://google.com');
-  }catch(e){response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ '+e)}
-  const options = {format:request.query.size||'A4', printBackground:typeof request.query.bg!=undefined?request.query.bg=='true':true}
-  const pdf = await page.pdf(options);
-  await browser.close();
-  response.type('application/pdf');
-  response.send(pdf);
-})();
-  else
-(async () => {
-  const browser = await pup.launch({
-      args: ['--no-sandbox']
-    });
-  const html = decodeURIComponent(request.query.html);
-  const page = await browser.newPage();
-  try{
-   await page.setContent(html);
-  }catch(e){response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ '+e)}
-  const options = {format:request.query.size||'A4', printBackground:typeof request.query.bg!=undefined?request.query.bg=='true':true}
-  const pdf = await page.pdf(options);
-  await browser.close();
-  response.type('application/pdf');
-  response.send(pdf);
-})();
-}catch(e){response.send('Whoops! Something went wrong, please try again! 🤦‍♂️ '+e)}});
+app.get(/.*/, pdfRequest);
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
